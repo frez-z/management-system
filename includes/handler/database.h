77a4ead5d710a292214d45b08eab5163215fa77e
Farez <82459956+FrezAmirul@users.noreply.github.com>
2022-01-16 23:47:43 +0800

    update database function

--- includes/handler/database.h
+++ includes/handler/database.h
@@ -1,4 +1,3 @@
-
 #include <sql/sqlite3.h>
 #include <stdio.h>
 
@@ -6,77 +5,239 @@ sqlite3 *db;
 sqlite3_stmt *stmt;
 char *db_error; // global db error message
 
+/* hold table information */
+typedef struct resTables {
+    unsigned int id;
+    unsigned short status; // 0 = unavailable, 1 = available
+    unsigned int resit_id; // customer resit id
+} resTables;
 
-/*
- * struct for restaurant information
- */
-typedef struct restaurantInfo {
-    const unsigned char *name;
-    const unsigned char * admin_name;
-    const unsigned char * password;
-} restaurantInfo;
+/* hold food or drink information */
+typedef struct resInventory {
+    unsigned int id; // unique id
+    const unsigned char *item_name;
+    double price;
+    unsigned int quantity;
+} resInventory;
 
+/* hold customer information */
+typedef struct resCustomer {
+    unsigned int resit_id; // unique id
+    int table;
+    double bill;
+    int status;
+} resCustomer;
 
-/*
- * open a new connection to database
- * argument: database file directory
- */
+/* open a new connection to database */
 void sql_open(char *fileDir){
     sqlite3_open(fileDir, &db);
 }
 
-
-/*
- * close the connection to database
- */
+/* close the connection to database */
 void sql_close(void){
     sqlite3_close(db);
 }
 
-
 /*
- * create new restaurant table and populated it with data.
- *
- * argument:
- *      name: name of the restaurant.
- *      admin_name: name created used to access administrator screen.
- *      admin_password: password created to access administrator screen.
+ * create new database for restaurant
+ * table created: 'items', 'tables' and 'customer'
  *
  * return value:
- *       0 = error creating database
  *       1 = execution success
- *      -1 = restaurant already exist
+ *       0 = execution fail
  */
-int createRestaurant(char *name, char *admin_name, char* admin_password){
-    int ic; // error handling variable
+int databaseSetup(void){
+    int result;
+    result = sqlite3_exec(db, "CREATE TABLE inventory (id INT PRIMARY KEY, item_name TEXT, price REAL, quantity INT);", NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0;
 
-    ic = sqlite3_exec(db,"CREATE TABLE IF NOT EXISTS restaurant (id INT PRIMARY KEY, restaurant_name VARCHAR(50), admin_name VARCHAR(20), admin_pass VARCHAR(20));", NULL, NULL, &db_error);
+    result = sqlite3_exec(db, "CREATE TABLE tables (id INT PRIMARY KEY, resit_id INT, status INT);", NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0;
 
-    if (ic != SQLITE_OK) return 0; // error creating table.
+    result = sqlite3_exec(db, "CREATE TABLE customer (resit_id INT PRIMARY KEY AUTOINCREMENT, bill REAL, status INT);", NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0;
 
-    char string[100];
-    sprintf(string,"INSERT INTO restaurant VALUES (1, '%s', '%s', '%s');", name, admin_name, admin_password);
-    ic = sqlite3_exec(db, string, NULL, NULL, &db_error);
-
-    if (ic != SQLITE_OK) return -1; // restaurant already exist
+    result = sqlite3_exec(db, "INSERT INTO customer VALUES (123455, NULL, NULL)", NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0; // create useless row to start autoincrement from 123455 instead of 0
 
     return 1;
 }
 
+// function for table ---------------------------------
 
 /*
- * fetch restaurant data without error handling
+ * populated table 'tables' with default value data
  *
  * argument:
- *      restaurantInfo struct pointer.
+ *      quantity: quantity table in the restaurant
+ *
+ * return value:
+ *       0 : execution success
+ *       return value >= 0 : rows of data successfully added into database
  */
-void fetchRestaurant(restaurantInfo *data){
-    stmt = NULL; // reinitialize stmt into NULL
+int tablesRegister(int quantity){
+    char string[100];
+    int result;
+    for (int i = 0; i < quantity; ++i) {
 
-    sqlite3_prepare_v2(db, "SELECT * FROM restaurant;", -1, &stmt, 0);
+        sprintf(string,"INSERT INTO tables VALUES (%d, NULL, 0);", i);
+        result =sqlite3_exec(db, string, NULL, NULL, &db_error);
+        if (result != SQLITE_OK) return i; // error adding table into database
+
+    }
+    return -1;
+}
+
+/*
+ * update 'tables' data
+ *
+ * return value:
+ *       1 : execution success
+ *       0 : execution fail
+ */
+int tablesUpdate(int table_id, int resit_id, int status){
+    char string[100];
+    int result;
+    sprintf(string,"UPDATE tables SET resit_id = %d, status = %d WHERE id = %d;", resit_id, status, table_id);
+    result = sqlite3_exec(db, string, NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0; // error adding table into database
+
+    return 1;
+}
+
+/*
+ * fetch information of selected table id
+ *
+ * return value:
+ *       resTables struct
+ */
+resTables tablesFetch(int table_id){
+    stmt = NULL;
+    char query[100];
+    sprintf(query,"SELECT * FROM tables WHERE id = %d;", table_id);
+    sqlite3_prepare_v2(db, query, -1, &stmt, 0);
     sqlite3_step(stmt);
+    resTables table;
+    table.id = sqlite3_column_int(stmt, 0);
+    table.resit_id = sqlite3_column_int(stmt, 1);
+    table.status = sqlite3_column_int(stmt, 2);
 
-    data->name = sqlite3_column_text(stmt, 1);
-    data->admin_name = sqlite3_column_text(stmt, 2);
-    data->password = sqlite3_column_text(stmt, 3);
+    return table;
+}
+
+// function for inventory -----------------------------
+
+int inventorAddyStock(const unsigned char *item_name, double price,unsigned int quantity){
+    char query[100];
+    int result;
+
+    sprintf(query,"INSERT INTO inventory (item_name, price, quantity) VALUES ('%s', %.2f, %d);", *item_name, price, quantity);
+    result = sqlite3_exec(db, query, NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0; // error adding table into database
+
+    return 1;
+}
+
+int inventorySetQuantity(short id, short quantity){
+    char query[100];
+    sprintf(query,"UPDATE inventory SET quantity = %d WHERE id = %d;", quantity, id);
+    int result = sqlite3_exec(db,query, NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0;
+    return 1;
+}
+
+int inventorySetPrice(short id, short quantity){
+    char query[100];
+    sprintf(query,"UPDATE inventory SET price = %d WHERE id = %d;", quantity, id);
+    int result = sqlite3_exec(db,query, NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0;
+    return 1;
+}
+
+resInventory inventoryFetch(int id){
+    stmt = NULL;
+    char query[100];
+    sprintf(query,"SELECT * FROM inventory WHERE id = %d;", id);
+    sqlite3_prepare_v2(db, query, -1, &stmt, 0);
+    sqlite3_step(stmt);
+    resInventory item;
+    item.id = sqlite3_column_int(stmt, 0);
+    item.item_name = sqlite3_column_text(stmt, 1);
+    item.price = sqlite3_column_double(stmt, 2);
+    item.quantity = sqlite3_column_int(stmt, 3);
+
+    return item;
+}
+
+// function for customer ------------------------------
+
+int customerGenerateBill(resCustomer *customer,double price, int table_id){
+    char query[100];
+    int result;
+
+    sprintf(query,"INSERT INTO customer (bill, status) VALUES (%.2f, 0);", price);
+    result = sqlite3_exec(db, query, NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0; // error adding table into database
+
+    stmt = NULL;
+
+    char second_query[] = "SELECT seq from sqlite_sequence where name=customer";
+    sqlite3_prepare_v2(db, second_query, -1, &stmt, 0);
+    sqlite3_step(stmt);
+    if (sqlite3_column_type(stmt, 0) == SQLITE_NULL) return 0;
+
+    int resit_id = sqlite3_column_int(stmt, 0);
+
+    int update_result = tablesUpdate(table_id, resit_id, 1);
+    if (!update_result) return 0;
+
+    customer->resit_id = resit_id;
+    customer->bill = price;
+    customer->status = 0;
+    customer->table = table_id;
+
+    return 1;
+}
+
+int customerInfo(resCustomer *customer, int resit_id){
+    stmt = NULL;
+    char query[100];
+    sprintf(query,"SELECT * FROM customer WHERE id = %d;", resit_id);
+    sqlite3_prepare_v2(db, query, -1, &stmt, 0);
+    sqlite3_step(stmt);
+    if (sqlite3_column_type(stmt, 0) == SQLITE_NULL) return 0;
+
+    customer->resit_id = sqlite3_column_int(stmt, 0);
+    customer->bill = sqlite3_column_int(stmt, 1);
+    customer->status = sqlite3_column_int(stmt, 2);
+
+    stmt = NULL;
+    sprintf(query,"SELECT id FROM tables WHERE resit_id = %d;", resit_id);
+    sqlite3_prepare_v2(db, query, -1, &stmt, 0);
+    sqlite3_step(stmt);
+    if (sqlite3_column_type(stmt, 0) == SQLITE_NULL) return -1;
+
+    customer->table = sqlite3_column_int(stmt, 0);
+
+    return 1;
+}
+
+int customerPayment(resCustomer *customer){
+    stmt = NULL;
+    char query[100];
+    sprintf(query,"UPDATE customer SET status = 0 WHERE id = %d;", customer->resit_id);
+
+    int result = sqlite3_exec(db, query, NULL, NULL, &db_error);
+    if (result != SQLITE_OK) return 0; // error update table in database
+
+    customer->status = 0;
+
+    sqlite3_prepare_v2(db, query, -1, &stmt, 0);
+    sqlite3_step(stmt);
+    if (sqlite3_column_type(stmt, 0) == SQLITE_NULL) return 0;
+
+    customer->resit_id = sqlite3_column_int(stmt, 0);
+    customer->bill = sqlite3_column_int(stmt, 1);
+    customer->status = sqlite3_column_int(stmt, 2);
+    return 1;
 }
